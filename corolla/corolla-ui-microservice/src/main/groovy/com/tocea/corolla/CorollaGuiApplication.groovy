3b408e74a9d766fc996d598be1595b12a41e7fd1
Sylvain Leroy <sleroy0@gmail.com>
2015-11-06 15:32:08 +0100

    Préparation KomeaCC

--- corolla/corolla-ui-microservice/src/main/groovy/com/tocea/corolla/CorollaGuiApplication.groovy
+++ corolla/corolla-ui-microservice/src/main/groovy/com/tocea/corolla/CorollaGuiApplication.groovy
@@ -4,15 +4,19 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.data.mongodb.config.EnableMongoAuditing
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableConfigurationProperties
+@EnableMongoRepositories
+@EnableMongoAuditing
 @EnableTransactionManagement
 //@EnableWebMvc
 class CorollaGuiApplication {
-
+	
 	public static void main(final String[] args) {
 		SpringApplication.run(CorollaGuiApplication.class, args)
 	}
--- komea-cc/komeacc-ui-microservice/src/main/groovy/com/tocea/corolla/CorollaGuiApplication.groovy
+++ komea-cc/komeacc-ui-microservice/src/main/groovy/com/tocea/corolla/CorollaGuiApplication.groovy
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableConfigurationProperties
-//@EnableJpaAuditing
-//@EnableJpaRepositories
 @EnableTransactionManagement
 //@EnableWebMvc
 class CorollaGuiApplication {
