9d78d039932cd96f6b82480e81e2afe26b871dc3
Sylvain Leroy <sleroy0@gmail.com>
2015-11-09 11:14:41 +0100

    [ksf, ldap] regeneration du test #19065

---
+++ ksf-commons/ksf-backend-users/src/test/java/fr/echoes/lab/ksf/users/security/config/LdapSecurityConfigurationTest.java
@@ -0,0 +1,49 @@
+package fr.echoes.lab.ksf.users.security.config;
+
+import org.junit.Test;
+
+public class LdapSecurityConfigurationTest {
+
+	@Test
+	public final void testBuildLdapContext() throws Exception {
+		// TODO
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testClearManagerDN() throws Exception {
+		// TODO
+		//		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testGetProviderUrl() throws Exception {
+		// TODO
+		//	throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testHasConfigurationProvided() throws Exception {
+		// TODO
+		//		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testHasUserDN() throws Exception {
+		// TODO
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testHasUserSearch() throws Exception {
+		// TODO
+		//		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testIsAuthRequired() throws Exception {
+		// TODO
+		//	throw new RuntimeException("not yet implemented");
+	}
+
+}
