553dc374d9fa7c5424045cc0b52b29cd582833a4
Sylvain Leroy <sleroy0@gmail.com>
2015-11-11 09:59:24 +0100

    [ksf-commons] refactoring  #19090

--- ksf-commons/ksf-backend-users/src/main/java/com/tocea/corolla/users/domain/User.java
+++ ksf-commons/ksf-backend-users/src/main/java/com/tocea/corolla/users/domain/User.java
@@ -26,7 +26,6 @@ import java.util.Locale;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.apache.commons.lang3.StringUtils;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
@@ -35,8 +34,6 @@ import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.security.ldap.userdetails.LdapUserDetails;
 
 import com.google.common.base.Strings;
-import com.tocea.corolla.users.service.EmailValidationService;
-import com.tocea.corolla.utils.domain.ObjectValidation;
 
 /**
  * This class declares a member that contributes to a repository. It is
@@ -49,74 +46,74 @@ import com.tocea.corolla.utils.domain.ObjectValidation;
  @Table(name = "users", indexes = { @Index(unique = true, name = "login_index", columnList = "login") })*/
 @Document(collection = "users")
 public class User implements Serializable {
-	
+
 	@Id
 	@Field("_id")
 	private String id;
-	
+
 	@NotBlank
 	@Size(min = 3, max = 30)
 	//@Column(nullable = false, length = 30)
 	private String login = "";
-	
+
 	@NotBlank
 	@Size(max = 40)
 	//@Column(nullable = false, length = 40)
 	private String firstName = "";
-	
+
 	@NotBlank
 	@Size(max = 40)
 	//@Column(nullable = false, length = 40)
 	private String lastName = "";
-	
+
 	@NotBlank
 	@Size(max = 128)
 	//@Column(nullable = false, length = 128)
 	@Email
 	private String email = "";
-	
+
 	@NotBlank
 	@Size(max = 256)
 	//@Column(nullable = false, length = 256)
 	private String password = "";
-	
+
 	@NotNull
 	//@Column(nullable = false)
 	private String roleId;
-	
+
 	@NotBlank
 	@Size(max = 10)
 	//@Column(nullable = false, length = 10)
 	private String locale = "en_GB";	//$NON-NLS-1$
-	
+
 	@Size(max = 50)
 	//@Column(nullable = true, length = 50)
 	private String activationToken = "";		//$NON-NLS-1$
-	
+
 	@NotNull
 	//@Column(nullable = false)
 	private Date createdTime;
-	
+
 	@Size(max = 256)
 	//@Column(nullable = false, length = 128)
 	private String salt = "";
-	
+
 	@NotNull
 	//@Column(nullable = false)
 	private boolean active = true;
-	
+
 	public User() {
 	}
-	
+
 	public User(final LdapUserDetails _principal) {
 		super();
 		setLogin(_principal.getUsername());
 		setFirstName(_principal.getUsername());
 		setPassword(_principal.getPassword());
-
-
+		
+		
 	}
-
+	
 	/**
 	 * Copy values if missing fields
 	 */
@@ -128,162 +125,147 @@ public class User implements Serializable {
 				&& Strings.isNullOrEmpty(lastName)) {
 			firstName = login;
 		}
-
+		
 		if (createdTime == null) {
 			createdTime = new Date();
 		}
-		
-		setLocaleIfNecessary();
-		
-	}
-	
-	/**
-	 * Checks some fields. If the Pojo is not valid, curate it.
-	 */
-	public void fixInformationsIfRequired() {
-		if (!new ObjectValidation().isValid(this)) {
-			// Attention : vérifier les valeurs de bornes définies dans les conditions de validation
-			firstName = Strings.isNullOrEmpty(firstName)? login : StringUtils.left(firstName, 40);
-			//
-			lastName = Strings.isNullOrEmpty(firstName)? "" : StringUtils.left(lastName, 40);
-			if (!new EmailValidationService().validateEmail(email)) {
-				email = login  + "@company.com";
-			}
 
-		}
+		setLocaleIfNecessary();
+
 	}
-	
+
+
 	/**
 	 * @return the activationToken
 	 */
 	public String getActivationToken() {
 		return activationToken;
 	}
-	
+
 	/**
 	 * @return the createdTime
 	 */
 	public Date getCreatedTime() {
 		return createdTime;
 	}
-	
+
 	/**
 	 * @return the email
 	 */
 	public String getEmail() {
 		return email;
 	}
-	
+
 	/**
 	 * @return the firstName
 	 */
 	public String getFirstName() {
 		return firstName;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	/**
 	 * @return the lastName
 	 */
 	public String getLastName() {
 		return lastName;
 	}
-	
+
 	/**
 	 * @return the locale
 	 */
 	public String getLocale() {
 		return locale;
 	}
-	
+
 	/**
 	 * @return the login
 	 */
 	public String getLogin() {
 		return login;
 	}
-	
+
 	/**
 	 * @return the password
 	 */
 	public String getPassword() {
 		return password;
 	}
-	
+
 	/**
 	 * @return the role_id
 	 */
 	public String getRoleId() {
 		return roleId;
 	}
-	
+
 	public String getSalt() {
 		return salt;
 	}
-	
+
 	/**
 	 * @return the active
 	 */
 	public boolean isActive() {
 		return active;
 	}
-	
+
 	/**
 	 * @param _activationToken the activationToken to set
 	 */
 	public void setActivationToken(final String _activationToken) {
 		activationToken = _activationToken;
 	}
-	
+
 	/**
 	 * @param _active the active to set
 	 */
 	public void setActive(final boolean _active) {
 		active = _active;
 	}
-	
+
 	/**
 	 * @param _createdTime the createdTime to set
 	 */
 	public void setCreatedTime(final Date _createdTime) {
 		createdTime = _createdTime;
 	}
-	
+
 	/**
 	 * @param _email the email to set
 	 */
 	public void setEmail(final String _email) {
 		email = _email;
 	}
-	
+
 	/**
 	 * @param _firstName the firstName to set
 	 */
 	public void setFirstName(final String _firstName) {
 		firstName = _firstName;
 	}
-	
+
 	public void setId(final String id) {
 		this.id = id;
 	}
-	
+
 	/**
 	 * @param _lastName the lastName to set
 	 */
 	public void setLastName(final String _lastName) {
 		lastName = _lastName;
 	}
-	
+
 	/**
 	 * @param _locale the locale to set
 	 */
 	public void setLocale(final String _locale) {
 		locale = _locale;
 	}
-	
+
 	/**
 	 * Set locale if necessary
 	 *
@@ -293,41 +275,41 @@ public class User implements Serializable {
 		if (Strings.isNullOrEmpty(getLocale())) {
 			setLocale(Locale.getDefault().toString());
 		}
-		
+
 	}
-	
+
 	/**
 	 * @param _login the login to set
 	 */
 	public void setLogin(final String _login) {
 		login = _login;
 	}
-	
+
 	/**
 	 * @param _password the password to set
 	 */
 	public void setPassword(final String _password) {
 		password = _password;
 	}
-	
+
 	public void setRole(final Role _role) {
 		roleId = _role.getId();
 	}
-	
+
 	/**
 	 * @param _role_id the role_id to set
 	 */
 	public void setRoleId(final String _role_id) {
 		roleId = _role_id;
 	}
-	
+
 	public void setSalt(final String salt) {
 		this.salt = salt;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "User{" + "id=" + id + ", login=" + login + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", roleId=" + roleId + ", locale=" + locale + ", activationToken=" + activationToken + ", createdTime=" + createdTime + ", active=" + active + '}';
 	}
-	
+
 }
--- ksf-commons/ksf-backend-users/src/main/java/fr/echoes/lab/ksf/users/security/auth/ldap/LdapUserConverterService.java
+++ ksf-commons/ksf-backend-users/src/main/java/fr/echoes/lab/ksf/users/security/auth/ldap/LdapUserConverterService.java
@@ -3,17 +3,21 @@ package fr.echoes.lab.ksf.users.security.auth.ldap;
 import java.text.MessageFormat;
 import java.util.Locale;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.security.ldap.userdetails.LdapUserDetails;
 import org.springframework.stereotype.Service;
 
+import com.google.common.base.Strings;
 import com.tocea.corolla.cqrs.gate.Gate;
 import com.tocea.corolla.users.commands.CreateUserCommand;
 import com.tocea.corolla.users.dao.IUserDAO;
 import com.tocea.corolla.users.domain.User;
 import com.tocea.corolla.users.dto.UserDto;
+import com.tocea.corolla.users.service.EmailValidationService;
+import com.tocea.corolla.utils.domain.ObjectValidation;
 
 import fr.echoes.lab.ksf.users.security.config.LdapSecurityConfiguration;
 import fr.echoes.lab.ksf.users.security.utils.SecurityLoggers;
@@ -28,16 +32,16 @@ import fr.echoes.lab.ksf.users.security.utils.SecurityLoggers;
 @Service
 public class LdapUserConverterService {
 	private final LdapTemplate ldapTemplate;
-	
+
 	private final LdapAttributesUserDetailsMapper userDetailsAttributeMapper;
-	
+
 	private final LdapSecurityConfiguration ldapSecurityConfiguration;
-	
+
 	private final Gate gate;
-	
+
 	@Autowired
 	private IUserDAO userDAO;
-	
+
 	@Autowired
 	public LdapUserConverterService(final LdapTemplate _ldapTemplate,
 			final LdapAttributesUserDetailsMapper _userDetailsAttributeMapper,
@@ -52,11 +56,11 @@ public class LdapUserConverterService {
 		Validate.notNull(_ldapSecurityConfiguration);
 		Validate.notNull(gate);
 	}
-	
+
 	public String buildQuery(final String _username) {
 		return MessageFormat.format(ldapSecurityConfiguration.getUserDetailsLookup(), _username);
 	}
-	
+
 	public UserDto convertLdapUserDetailsIntoDatabaseUser(final LdapUserDetails _principal) {
 		try {
 			final User findUserByLogin = userDAO.findUserByLogin(_principal.getUsername());
@@ -78,7 +82,24 @@ public class LdapUserConverterService {
 		userDto.setLocale(Locale.getDefault().toString());
 		return userDto;
 	}
-	
+
+	/**
+	 * Checks some fields. If the Pojo is not valid, curate it.
+	 * @param _userLdapInformation
+	 */
+	public void fixInformationsIfRequired(final User _userLdapInformation) {
+		if (!new ObjectValidation().isValid(this)) {
+			// Attention : vérifier les valeurs de bornes définies dans les conditions de validation
+			_userLdapInformation.setFirstName(Strings.isNullOrEmpty(_userLdapInformation.getFirstName())? _userLdapInformation.getLogin() : StringUtils.left(_userLdapInformation.getFirstName(), 40));
+			//
+			_userLdapInformation.setLastName(Strings.isNullOrEmpty(_userLdapInformation.getLastName())? "" : StringUtils.left(_userLdapInformation.getLastName(), 40));
+			if (!new EmailValidationService().validateEmail(_userLdapInformation.getEmail())) {
+				_userLdapInformation.setEmail(_userLdapInformation.getLogin()  + "@company.com");
+			}
+			
+		}
+	}
+
 	/**
 	 * Try to retrieve informations about an user from the ldap.
 	 *
@@ -94,7 +115,7 @@ public class LdapUserConverterService {
 			userLdapInformation = ldapTemplate.lookup(queryToObtainUser, userDetailsAttributeMapper);
 			if (userLdapInformation != null) {
 				SecurityLoggers.LDAP_LOGGER.info("Found User details from LDAP {}", userLdapInformation);
-				userLdapInformation.fixInformationsIfRequired();
+				fixInformationsIfRequired(userLdapInformation);
 				gate.dispatch(new CreateUserCommand(userLdapInformation));
 			} else {
 				SecurityLoggers.LDAP_LOGGER.debug("LDAP did not provide any user information for {}", _username);
@@ -103,6 +124,7 @@ public class LdapUserConverterService {
 			SecurityLoggers.LDAP_LOGGER.error("LDAP Lookup for {} met an exception", _username, e);
 		}
 		return userLdapInformation;
-		
+
 	}
+
 }
