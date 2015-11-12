f6c9560fb5d30c02d2e6f26761d0ceb84257bc88
dmichel <david.michel@tocea.com>
2015-11-12 14:22:30 +0100

    [komeacc] ajout d'un point d'extension pour gérer le cycle de vie d'un projet dans le plugin Foreman #19042

---
+++ komea-cc/komeacc-plugins/komeacc-foreman-plugin/src/main/java/fr/echoes/lab/ksf/cc/plugins/foreman/extensions/ForemanProjectLifeCycleExtension.java
@@ -0,0 +1,40 @@
+package fr.echoes.lab.ksf.cc.plugins.foreman.extensions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.echoes.lab.ksf.extensions.annotations.Extension;
+import fr.echoes.lab.ksf.extensions.projects.IProjectLifecycleExtension;
+import fr.echoes.lab.ksf.extensions.projects.ProjectDto;
+
+@Extension
+public class ForemanProjectLifeCycleExtension implements IProjectLifecycleExtension {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ForemanProjectLifeCycleExtension.class);
+	
+	@Override
+	public void notifyCreatedProject(ProjectDto _project) {
+		
+		LOGGER.info("[foreman] project creation detected : "+_project.getKey());
+		
+	}
+
+	@Override
+	public void notifyDeletedProject(ProjectDto _project) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyDuplicatedProject(ProjectDto _project) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyUpdatedProject(ProjectDto _project) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
