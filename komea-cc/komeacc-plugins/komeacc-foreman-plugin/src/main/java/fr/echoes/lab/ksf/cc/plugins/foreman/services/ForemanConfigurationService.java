baf369dac4a2ae310cb808ee9f921ac73e889ed6
RaphaelTocea <rgalerme@Tera>
2015-11-16 18:31:35 +0100

    #19058 preparer pour test vm

--- komea-cc/komeacc-plugins/komeacc-foreman-plugin/src/main/java/fr/echoes/lab/ksf/cc/plugins/foreman/services/ForemanConfigurationService.java
+++ komea-cc/komeacc-plugins/komeacc-foreman-plugin/src/main/java/fr/echoes/lab/ksf/cc/plugins/foreman/services/ForemanConfigurationService.java
@@ -3,28 +3,31 @@ package fr.echoes.lab.ksf.cc.plugins.foreman.services;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("foremanConfiguration")
 public class ForemanConfigurationService {
 
-	@Value("${ksf.foreman.url}")
-	private String url;
+    @Value("${ksf.foreman.url}")
+    private String url;
 
-	@Value("${ksf.foreman.username}")
-	private String username;
+    @Value("${ksf.foreman.username}")
+    private String username;
 
-	@Value("${ksf.foreman.password}")
-	private String password;
+    @Value("${ksf.foreman.password}")
+    private String password;
 
-	public String getForemanUrl() {
-		return this.url;
-	}
+    public String getForemanUrl() {
+        if ('/' == url.charAt(url.length() - 1)) {
+            url = url.substring(0, url.length() - 1);
+        }
+        return this.url;
+    }
 
-	public String getForemanUsername() {
-		return this.username;
-	}
+    public String getForemanUsername() {
+        return this.username;
+    }
 
-	public String getForemanPassword() {
-		return this.password;
-	}
+    public String getForemanPassword() {
+        return this.password;
+    }
 
 }
--- komea-cc/komeacc-ui-microservice/src/main/resources/static/js/local/guilocal.js
+++ komea-cc/komeacc-ui-microservice/src/main/resources/static/js/local/guilocal.js
@@ -12,14 +12,37 @@ function donner_focus(chp)
 function loginForman() {
     var login = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    alert('login '+login );
+//    alert('login '+login );
+    IE = window.ActiveXObject ? true : false;
+    MOZ = window.sidebar ? true : false;
+    alert(pageData.foremanUrl)
+
+//    if (IE)
+//    {
+//        var contenu_iframe = window.frames['iframe_article'].document.getElementsByName('authenticity_token');
+//    } else {
+//        var contenu_iframe = document.getElementById("formanFrame").contentWindow.document.getElementsByName('authenticity_token');
+//
+//    }
+//
+//    var keyAuthen = contenu_iframe[0].value;
+//    alert(keyAuthen);
 //    var frameContent = document.getElementById('formanFrame').contentDocument.body.innerHTML;
-//    var frameContent = document.getElementById('formanFrame').contentWindow.body.innerHTML;
+
 //    $.get("https://https://passerelle:8443/users/login", function (data) {
 ////        
 ////        
 //        alert(data);
 //    });
+
+
+    var parametres = [{clé: "login[login]", valeur: login}, {clé: "login[password]", valeur: password}, {clé: "authenticity_token", valeur: keyAuthen}];
+//    post_en_url(pageData.foremanUrl+"/users/login", parametres);
+//    monobjet = window.frames['formanFrame'].document;
+//    alert(monobjet);
+//    alert('dd');
+}
+
 function post_en_url(url, parametres) {
 //Création dynamique du formulaire
     var form = document.createElement('form');
@@ -37,16 +60,10 @@ function post_en_url(url, parametres) {
     }
 //Ajout du formulaire à la page et soumission du formulaire
     document.body.appendChild(form);
-   var result = form.submit();
-   alert(result);
+    var result = form.submit();
+    alert(result);
 }
 
-var parametres = [{clé:"login[login]", valeur:login}, {clé:"login[password]", valeur:password}, {clé:"authenticity_token", valeur:"0U6GaVPli178sm2qeXb4KD8fVLn5KpTLtrR1ZJf2ghU="}];
-    post_en_url("https://10.1.10.9/users/login",parametres)
-//    monobjet = window.frames['formanFrame'].document;
-//    alert(monobjet);
-//    alert('dd');
-}
 ;
 
 
