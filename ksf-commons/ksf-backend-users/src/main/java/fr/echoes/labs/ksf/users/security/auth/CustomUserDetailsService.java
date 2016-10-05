9c71832bbb0e33ff8a7e1c8376318a85685b1a5c
Sebastien Carreau <sebastien.carreau@tocea.com>
2016-10-05 18:09:57 +0200

    #20968 : Les utilisateurs dont le login est en MAJUSCULE dans LDAP font exploser la création de projet dans la partie Foreman

--- ksf-commons/ksf-backend-users/src/main/java/fr/echoes/labs/ksf/users/security/auth/CustomUserDetailsService.java
+++ ksf-commons/ksf-backend-users/src/main/java/fr/echoes/labs/ksf/users/security/auth/CustomUserDetailsService.java
@@ -1,62 +1,43 @@
 package fr.echoes.labs.ksf.users.security.auth;
 
+import fr.echoes.labs.ksf.users.security.config.AppUserDetails;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import fr.echoes.labs.ksf.users.security.config.AppUserDetails;
 
 /**
  * Authenticate a user from the database.
  */
 public class CustomUserDetailsService implements AuthenticationUserDetailsService<CasAssertionAuthenticationToken> {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(CustomUserDetailsService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomUserDetailsService.class);
 
-	private Set<String> admins;
+    public CustomUserDetailsService() {
+        super();
+    }
 
-	public CustomUserDetailsService() {
-		super();
-	}
+    @Override
+    public UserDetails loadUserDetails(CasAssertionAuthenticationToken token) throws UsernameNotFoundException {
+        final String login = token.getPrincipal().toString();
 
-	/**
-	 * @param admins
-	 */
-	public CustomUserDetailsService(Set<String> admins) {
-		super();
-		this.admins = admins;
-	}
+        LOGGER.debug("Authenticating '{}'", login);
+        final List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
 
-	@Override
-	public UserDetails loadUserDetails(CasAssertionAuthenticationToken token) throws UsernameNotFoundException {
-		final String login = token.getPrincipal().toString();
-		final String lowercaseLogin = login.toLowerCase();
+        grantedAuthorities.add(new GrantedAuthority() {
+            private static final long serialVersionUID = 1L;
 
-		LOGGER.debug("Authenticating '{}'", login);
-		final List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+            @Override
+            public String getAuthority() {
+                return "ROLE_USER";
+            }
+        });
 
-		if (this.admins != null && this.admins.contains(lowercaseLogin)) {
-			grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-		} else {
-			grantedAuthorities.add(new GrantedAuthority() {
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public String getAuthority() {
-					return "ROLE_USER";
-				}
-			});
-		}
-
-		return new AppUserDetails(lowercaseLogin, grantedAuthorities);
-	}
+        return new AppUserDetails(login, grantedAuthorities);
+    }
 }
--- ksf-commons/ksf-backend-users/src/main/java/fr/echoes/labs/ksf/users/security/auth/InternalUserAuthenticationManager.java
+++ ksf-commons/ksf-backend-users/src/main/java/fr/echoes/labs/ksf/users/security/auth/InternalUserAuthenticationManager.java
@@ -5,9 +5,6 @@
  */
 package fr.echoes.labs.ksf.users.security.auth;
 
-import java.util.Collections;
-import java.util.Set;
-
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,77 +33,72 @@ import fr.echoes.labs.ksf.users.security.utils.SecurityLoggers;
 @Service
 public class InternalUserAuthenticationManager implements IUserAuthenticationManager {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(InternalUserAuthenticationManager.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(InternalUserAuthenticationManager.class);
 
-	@Autowired
-	private UserDetailsService userDetailsService;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-	@Autowired
-	private LdapSecurityConfiguration ldapSecurity;
+    @Autowired
+    private LdapSecurityConfiguration ldapSecurity;
 
-	@Autowired
-	private SecurityConfiguration security;
+    @Autowired
+    private SecurityConfiguration security;
 
-	@Autowired()
-	private ContextSource ldapContextSource;
+    @Autowired()
+    private ContextSource ldapContextSource;
 
-	@Override
-	public void initializeKsfAuth(final AuthenticationManagerBuilder auth) throws Exception {
-		// also see LdapAuthenticationProviderConfigurer and @code ContextSourceBuilder
+    @Override
+    public void initializeKsfAuth(final AuthenticationManagerBuilder auth) throws Exception {
+        // also see LdapAuthenticationProviderConfigurer and @code ContextSourceBuilder
 
-		//		auth.inMemoryAuthentication().withUser("user").password("password1")
-		//				.roles("GUI")
-		if (this.ldapSecurity.hasConfigurationProvided()) {
-			LOGGER.info("Initialization of LDAP Authentication provider");
-			initializeLdapAuthenticationManager(auth);
-		}
-		LOGGER.info("Initialization of DAO Authentication provider");
-		final DaoAuthenticationProvider userEmbeddedAuthenticationProvider = new DaoAuthenticationProvider();
-		userEmbeddedAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder(this.security.getPasswordStrength()));
-		userEmbeddedAuthenticationProvider.setUserDetailsService(this.userDetailsService);
-		auth.authenticationProvider(userEmbeddedAuthenticationProvider);
+        //		auth.inMemoryAuthentication().withUser("user").password("password1")
+        //				.roles("GUI")
+        if (this.ldapSecurity.hasConfigurationProvided()) {
+            LOGGER.info("Initialization of LDAP Authentication provider");
+            initializeLdapAuthenticationManager(auth);
+        }
+        LOGGER.info("Initialization of DAO Authentication provider");
+        final DaoAuthenticationProvider userEmbeddedAuthenticationProvider = new DaoAuthenticationProvider();
+        userEmbeddedAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder(this.security.getPasswordStrength()));
+        userEmbeddedAuthenticationProvider.setUserDetailsService(this.userDetailsService);
+        auth.authenticationProvider(userEmbeddedAuthenticationProvider);
 
-		// keep the credentials in the session for using them against a REST API
-		auth.eraseCredentials(false);
-	}
+        // keep the credentials in the session for using them against a REST API
+        auth.eraseCredentials(false);
+    }
 
-	private void initializeLdapAuthenticationManager(final AuthenticationManagerBuilder auth) throws Exception {
-		LdapAuthenticationProviderConfigurer<AuthenticationManagerBuilder> ldapAuth = auth.ldapAuthentication();
+    private void initializeLdapAuthenticationManager(final AuthenticationManagerBuilder auth) throws Exception {
+        LdapAuthenticationProviderConfigurer<AuthenticationManagerBuilder> ldapAuth = auth.ldapAuthentication();
 
-		SecurityLoggers.LDAP_LOGGER.info("Enabling LDAP Authentication on server {}", this.ldapSecurity.getUrl());
-		this.ldapSecurity.clearManagerDN();
-		Validate.notNull(this.ldapContextSource);
-		ldapAuth = ldapAuth.contextSource((BaseLdapPathContextSource) this.ldapContextSource);
+        SecurityLoggers.LDAP_LOGGER.info("Enabling LDAP Authentication on server {}", this.ldapSecurity.getUrl());
+        this.ldapSecurity.clearManagerDN();
+        Validate.notNull(this.ldapContextSource);
+        ldapAuth = ldapAuth.contextSource((BaseLdapPathContextSource) this.ldapContextSource);
 
-		if (this.ldapSecurity.hasUserDN()) {
-			SecurityLoggers.LDAP_LOGGER.info("Ldap Security base on UserDN pattern");
-			ldapAuth = ldapAuth
-					.userDnPatterns(this.ldapSecurity.getUserDnPattern());
+        if (this.ldapSecurity.hasUserDN()) {
+            SecurityLoggers.LDAP_LOGGER.info("Ldap Security base on UserDN pattern");
+            ldapAuth = ldapAuth
+                    .userDnPatterns(this.ldapSecurity.getUserDnPattern());
 
-		} else if (this.ldapSecurity.hasUserSearch()) {
-			SecurityLoggers.LDAP_LOGGER.info("Ldap Security base on UserDN pattern");
-			ldapAuth = ldapAuth
-					.userSearchBase(this.ldapSecurity.getUserSearchBase())
-					.userSearchFilter(this.ldapSecurity.getUserSearchFilter());
-		}
-		if (!this.ldapSecurity.getGroupSearchBase().isEmpty()) {
-			SecurityLoggers.LDAP_LOGGER.info("Setting groupSearchBase={}", this.ldapSecurity.getGroupSearchBase());
-			ldapAuth = ldapAuth.groupSearchBase(this.ldapSecurity.getGroupSearchBase());
-		}
-		if (!this.ldapSecurity.getGroupRoleAttribute().isEmpty()) {
-			SecurityLoggers.LDAP_LOGGER.info("Setting groupRoleAttribute={}", this.ldapSecurity.getGroupRoleAttribute());
-			ldapAuth = ldapAuth.groupRoleAttribute(this.ldapSecurity.getGroupRoleAttribute());
-		}
-		if (!this.ldapSecurity.getGroupSearchFilter().isEmpty()) {
-			SecurityLoggers.LDAP_LOGGER.info("Setting groupSearchFilter={}", this.ldapSecurity.getGroupSearchFilter());
-			ldapAuth = ldapAuth.groupSearchFilter(this.ldapSecurity.getGroupSearchFilter());
+        } else if (this.ldapSecurity.hasUserSearch()) {
+            SecurityLoggers.LDAP_LOGGER.info("Ldap Security base on UserDN pattern");
+            ldapAuth = ldapAuth
+                    .userSearchBase(this.ldapSecurity.getUserSearchBase())
+                    .userSearchFilter(this.ldapSecurity.getUserSearchFilter());
+        }
+        if (!this.ldapSecurity.getGroupSearchBase().isEmpty()) {
+            SecurityLoggers.LDAP_LOGGER.info("Setting groupSearchBase={}", this.ldapSecurity.getGroupSearchBase());
+            ldapAuth = ldapAuth.groupSearchBase(this.ldapSecurity.getGroupSearchBase());
+        }
+        if (!this.ldapSecurity.getGroupRoleAttribute().isEmpty()) {
+            SecurityLoggers.LDAP_LOGGER.info("Setting groupRoleAttribute={}", this.ldapSecurity.getGroupRoleAttribute());
+            ldapAuth = ldapAuth.groupRoleAttribute(this.ldapSecurity.getGroupRoleAttribute());
+        }
+        if (!this.ldapSecurity.getGroupSearchFilter().isEmpty()) {
+            SecurityLoggers.LDAP_LOGGER.info("Setting groupSearchFilter={}", this.ldapSecurity.getGroupSearchFilter());
+            ldapAuth = ldapAuth.groupSearchFilter(this.ldapSecurity.getGroupSearchFilter());
 
-		}
+        }
 
-	}
-
-	@Override
-	public Set<String> adminList() {
-		return Collections.<String>emptySet();
-	}
+    }
 }
