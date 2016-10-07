4f297b77ded7f0cf331059a6a7069fabf0d57f07
Sebastien Carreau <sebastien.carreau@tocea.com>
2016-10-07 14:14:04 +0200

    #20966 : Une tâche affectée ne doit être visible/démarrable que par la personne à qui elle est affectée au niveau de l'overview

--- komea-foundation/komea-foundation-ui-microservice/src/main/java/fr/echoes/labs/ksf/cc/extmanager/projects/ui/ProjectInformationManager.java
+++ komea-foundation/komea-foundation-ui-microservice/src/main/java/fr/echoes/labs/ksf/cc/extmanager/projects/ui/ProjectInformationManager.java
@@ -1,17 +1,7 @@
 package fr.echoes.labs.ksf.cc.extmanager.projects.ui;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.google.common.collect.Lists;
 import com.tocea.corolla.products.domain.Project;
-
 import fr.echoes.labs.ksf.cc.extensions.services.project.TicketStatus;
 import fr.echoes.labs.ksf.cc.extensions.services.project.features.IProjectFeatues;
 import fr.echoes.labs.ksf.cc.extensions.services.project.features.IProjectFeature;
@@ -21,6 +11,14 @@ import fr.echoes.labs.ksf.cc.extensions.services.project.versions.ProjectVersion
 import fr.echoes.labs.ksf.cc.releases.dao.IReleaseDAO;
 import fr.echoes.labs.ksf.cc.releases.model.Release;
 import fr.echoes.labs.ksf.cc.releases.model.ReleaseState;
+import fr.echoes.labs.ksf.users.security.api.CurrentUserService;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * This service allows to obtain project information returned by the plug-ins.
@@ -32,91 +30,121 @@ import fr.echoes.labs.ksf.cc.releases.model.ReleaseState;
 @Service("projectInformationManager")
 public class ProjectInformationManager {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ProjectInformationManager.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProjectInformationManager.class);
 
-	@Autowired(required = false)
-	private IProjectVersions[] versions;
+    @Autowired(required = false)
+    private IProjectVersions[] versions;
 
-	@Autowired(required = false)
-	private IProjectFeatues[] features;
+    @Autowired(required = false)
+    private IProjectFeatues[] features;
 
-	@Autowired
-	IReleaseDAO releaseDao;
+    @Autowired
+    IReleaseDAO releaseDao;
 
-	/**
-	 * Returns the versions for this project.
-	 *
-	 * @param projectName the project name.
-	 * @return a list of versions.
-	 */
-	public List<IProjectVersion> getVersions(Project project) {
+    @Autowired
+    CurrentUserService currentUserService;
 
-		if (this.versions == null) {
-			LOGGER.info("getVersions : no versions.");
-			return Lists.newArrayList();
-		}
+    /**
+     * Returns the versions for this project.
+     *
+     * @param projectName the project name.
+     * @return a list of versions.
+     */
+    public List<IProjectVersion> getVersions(Project project) {
 
-		final String projectName = project.getName();
+        if (this.versions == null) {
+            LOGGER.info("getVersions : no versions.");
+            return Lists.newArrayList();
+        }
 
-		final List<IProjectVersion> result = new ArrayList<IProjectVersion>();
-		final List<Release> startedReleases = this.releaseDao.findAll();
+        final String projectName = project.getName();
 
-		for (final IProjectVersions projectRelease : this.versions) {
-			final List<IProjectVersion> releases = projectRelease.getVersions(projectName);
-			if (releases != null) {
-				for (final IProjectVersion r : releases) {
-					for (final Release startedRelease : startedReleases) {
-						if (StringUtils.equals(startedRelease.getReleaseId(), r.getId())) {
-							if (startedRelease.getState() == ReleaseState.FINISHED) {
-								((ProjectVersion) r).setStatus(TicketStatus.FINISHED);
-							} else if (startedRelease.getState() == ReleaseState.STARTED) {
-								((ProjectVersion) r).setStatus(TicketStatus.CREATED);
-							}
-							break;
-						}
-					}
-				}
-				result.addAll(releases);
-			}
-		}
-		LOGGER.info("getVersions : the project \"{}\" has {} versions.", projectName, result.size());
-		return result;
-	}
+        final List<IProjectVersion> result = new ArrayList<IProjectVersion>();
+        final List<Release> startedReleases = this.releaseDao.findAll();
 
-	private boolean isStartedRelease(IProjectVersion release, List<Release> startedReleases) {
-		for (final Release startedRelease : startedReleases) {
-			if (StringUtils.equals(startedRelease.getReleaseId(), release.getId())) {
-				return startedRelease.getState() != ReleaseState.FINISHED;
-			}
-		}
-		return false;
-	}
+        for (final IProjectVersions projectRelease : this.versions) {
+            final List<IProjectVersion> releases = projectRelease.getVersions(projectName);
+            if (releases != null) {
+                for (final IProjectVersion r : releases) {
+                    for (final Release startedRelease : startedReleases) {
+                        if (StringUtils.equals(startedRelease.getReleaseId(), r.getId())) {
+                            if (startedRelease.getState() == ReleaseState.FINISHED) {
+                                ((ProjectVersion) r).setStatus(TicketStatus.FINISHED);
+                            } else if (startedRelease.getState() == ReleaseState.STARTED) {
+                                ((ProjectVersion) r).setStatus(TicketStatus.CREATED);
+                            }
+                            break;
+                        }
+                    }
+                }
+                result.addAll(releases);
+            }
+        }
+        LOGGER.info("getVersions : the project \"{}\" has {} versions.", projectName, result.size());
+        return result;
+    }
 
-	/**
-	 * Returns the features for this project.
-	 *
-	 * @param projectName the project name.
-	 * @return a list of features.
-	 */
-	public List<IProjectFeature> getFeatures(Project project) {
+    private boolean isStartedRelease(IProjectVersion release, List<Release> startedReleases) {
+        for (final Release startedRelease : startedReleases) {
+            if (StringUtils.equals(startedRelease.getReleaseId(), release.getId())) {
+                return startedRelease.getState() != ReleaseState.FINISHED;
+            }
+        }
+        return false;
+    }
 
-		if (this.features == null) {
-			LOGGER.info("getFeatures : no features.");
-			return Lists.newArrayList();
-		}
+    /**
+     * Returns the features for this project.
+     *
+     * @param projectName the project name.
+     * @return a list of features.
+     */
+    public List<IProjectFeature> getFeatures(Project project) {
 
-		final String projectName = project.getName();
+        if (this.features == null) {
+            LOGGER.info("getFeatures : no features.");
+            return Lists.newArrayList();
+        }
 
-		final List<IProjectFeature> result = new ArrayList<IProjectFeature>();
+        final String projectName = project.getName();
 
-		for (final IProjectFeatues projectFeature : this.features) {
-			final List<IProjectFeature> features = projectFeature.getFeatures(projectName);
-			if (features != null) {
-				result.addAll(features);
-			}
-		}
-		LOGGER.info("getVersions : the project \"{}\" has {} features.", projectName, result.size());
-		return result;
-	}
+        final List<IProjectFeature> result = new ArrayList<IProjectFeature>();
+
+        for (final IProjectFeatues projectFeature : this.features) {
+            final List<IProjectFeature> features = projectFeature.getFeatures(projectName);
+            if (features != null) {
+                result.addAll(features);
+            }
+        }
+        LOGGER.info("getVersions : the project \"{}\" has {} features.", projectName, result.size());
+        return result;
+    }
+
+    public List<IProjectFeature> getFeaturesOfCurrentUser(Project project) {
+
+        if (this.features == null) {
+            LOGGER.info("getFeaturesOfCurrentUser : no features.");
+            return Lists.newArrayList();
+        }
+        final String login = this.currentUserService.getCurrentUserLogin();
+
+        final String projectName = project.getName();
+
+        final List<IProjectFeature> result = new ArrayList<IProjectFeature>();
+
+        for (final IProjectFeatues projectFeature : this.features) {
+            final List<IProjectFeature> features = projectFeature.getFeatures(projectName);
+            if (features != null) {
+                for (final IProjectFeature feature : features) {
+                    final String assignee = feature.getAssignee();
+                    if (assignee.isEmpty() || assignee.toLowerCase().equals(login.toLowerCase())) {
+                        result.add(feature);
+                    }
+                }
+            }
+        }
+        LOGGER.info("getFeaturesOfCurrentUser : the project \"{}\" has {} features.", projectName, result.size());
+        return result;
+    }
 
 }
