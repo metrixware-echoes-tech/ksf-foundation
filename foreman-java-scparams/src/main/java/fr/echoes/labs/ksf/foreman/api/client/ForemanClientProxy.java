9a25e0b1e8af9c82ffddb6420d5515488a2d9fb9
David <contact@davidmichel.fr>
2016-11-24 17:36:01 +0100

    [foreman-java-scparams] add cache on smart variables API

--- foreman-java-scparams/src/main/java/fr/echoes/labs/ksf/foreman/api/client/ForemanClientProxy.java
+++ foreman-java-scparams/src/main/java/fr/echoes/labs/ksf/foreman/api/client/ForemanClientProxy.java
@@ -2,13 +2,17 @@ package fr.echoes.labs.ksf.foreman.api.client;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Collection;
 import java.util.Map;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Maps;
 
 import fr.echoes.labs.ksf.foreman.api.model.ForemanHostGroup;
 import fr.echoes.labs.ksf.foreman.api.model.PuppetClass;
 import fr.echoes.labs.ksf.foreman.api.model.SmartClassParameter;
+import fr.echoes.labs.ksf.foreman.api.model.SmartVariable;
 import fr.echoes.labs.ksf.foreman.api.utils.PuppetClassUtils;
 import fr.echoes.labs.ksf.foreman.api.utils.ScParamsUtils;
 
@@ -17,6 +21,7 @@ public class ForemanClientProxy extends ForemanClient {
 	private Map<Integer, SmartClassParameter> smartClassParameters = Maps.newHashMap();
 	private Map<Integer, PuppetClass> puppetClasses = Maps.newHashMap();
 	private Map<String, ForemanHostGroup> hostGroups = Maps.newHashMap();
+	private Map<String, SmartVariable> smartVariables = Maps.newHashMap();
 	
 	public ForemanClientProxy(final String url, final String username, final String password) throws MalformedURLException {
 		super(url, username, password);
@@ -83,6 +88,43 @@ public class ForemanClientProxy extends ForemanClient {
 		}
 
 		return this.hostGroups.get(hostGroupName);
+	}
+	
+	@Override
+	public SmartVariable getSmartVariableByName(final String variable) throws IOException {
+		
+		if (!this.smartVariables.containsKey(variable)) {
+			final SmartVariable smartVariable = super.getSmartVariableByName(variable);
+			this.smartVariables.put(variable, smartVariable);
+		}
+		
+		return this.smartVariables.get(variable);
+	}
+	
+	@Override
+	public SmartVariable getSmartVariable(final Integer id) throws IOException {
+		
+		// look for the smart variable in the cache
+		final Collection<SmartVariable> matches = Collections2.filter(this.smartVariables.values(), new Predicate<SmartVariable>() {
+			@Override
+			public boolean apply(final SmartVariable var) {
+				return id.equals(var.getId());
+			}
+		});
+		
+		if (!matches.isEmpty()) {
+			return matches.iterator().next();
+		}
+		
+		// look for the smart variable through Foreman API
+		final SmartVariable smartVariable = super.getSmartVariable(id);
+		
+		if (smartVariable != null) {
+			// store the smart variable in the cache
+			this.smartVariables.put(smartVariable.getVariable(), smartVariable);
+		}
+		
+		return smartVariable;
 	}
 	
 }
